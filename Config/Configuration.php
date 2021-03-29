66e89857fe7b37a4c2e0bafb3252c80aa1753c61
brit45 <blanc45@livehost.fr>
2021-03-29 17:06:06 +0200

    MISE À JOURS Modification du formulaire de configurations

--- Config/Configuration.php
+++ Config/Configuration.php
@@ -1,5 +1,6 @@
 <?php
 
+
 include_once "routes.php";
 
 class Config {
@@ -11,37 +12,31 @@ class Config {
     }
 };
 
-Config::loadConf([
+function loadFile(string $link) {
 
-    "DataBase" => [
+    $y = json_decode(file_get_contents($link.'.json'),true);
+    
+    foreach($y as $k => $v) {
 
-        "DataBase_Type"     => "mysql",
+        if(preg_match_all("/([@])([a-zA-Z0-9\/.]+)/",$v,$match,PREG_SET_ORDER)) {
 
-        // if Type is MySQL
+            if($l = file_get_contents(CONFIG.$match[0][2].'.json',true)) {
+                $y[$k] = json_decode($l,true,1024);
+            }
+        }
         
-        "DataBase_Login"    => null,
-        "DataBase_Addr"     => null,
-        "DataBase_Pass"     => null,
-        "DataBase_Name"     => null,
-        "DataBase_OPtion"   => [
+        foreach($v as $ke => $va) {
 
-            PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_OBJ,
-            PDO::ATTR_ERRMODE               => PDO::ERRMODE_SILENT,
-            PDO::PARAM_STR_CHAR             => "utf-8"
+            if(preg_match_all("/([@])([a-zA-Z0-9\/.]+)/",$va,$match,PREG_SET_ORDER)) {
 
-        ],
+                if($le = file_get_contents(CONFIG.$match[0][2].'.json',true)) {
+                    $k[$ke] = json_decode($le,true,1024);
+                }
+            }
+        }
+    }
+    return (array) $y;
+}
 
-        // ------------------
 
-        // if Type is liteSQL
-
-        "DataBase_Path"     => null
-
-        // ------------------
-
-    ],
-
-    "Debug"             => true,
-    "Layout_Default"    => "Default",
-    "Layout_Admin"      => ""
-]);
+Config::loadConf(loadFile(CONFIG.'config'));
